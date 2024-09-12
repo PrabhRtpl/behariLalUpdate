b7fcd4332f0cb6262e564c30386bebeea228b6f8
PrabhRtpl <prabhjot@rtpltech.com>
2024-09-12 16:28:01 +0530

    products

--- include/navbar.php
+++ include/navbar.php
@@ -13,7 +13,7 @@
           </a>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="dropProducts" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle" href="products.php" id="dropProducts" role="button" data-bs-toggle="dropdown" aria-expanded="false" onclick="window.location.href='products.php';">
            Products
           </a>
           <ul class="dropdown-menu" aria-labelledby="dropProducts">
---
+++ products.php
@@ -0,0 +1,100 @@
+<?php include 'include/head.php'; ?>
+<?php include 'include/navbar.php'; ?>
+<!-- main-area -->
+<main class="main-area fix">
+
+    <!-- breadcrumb-area -->
+    <div class="breadcrumb__area breadcrumb__bg" data-background="assets/img/banner/products.jpg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcrumb__content">
+                        <h2 class="title">Our Products</h2>
+                        <nav class="breadcrumb">
+                            <span property="itemListElement" typeof="ListItem">
+                                <a href="index.html">Home</a>
+                            </span>
+                            <span class="breadcrumb-separator">/</span>
+                            <span property="itemListElement" typeof="ListItem">Our Products</span>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- breadcrumb-area-end -->
+
+    <!-- services-area -->
+    <section class="featWrap container-fluid section-pt-120 section-pb-95">
+        <h6 class="subHead">What We Offer</h6>
+        <h2 class="secHead">Popular Products</h2>
+        <div class="row features">
+            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                <div class="product" data-aos="fade-right" data-aos-duration="1500">
+                    <img src="assets/img/metalRoll.png" alt="worker icon">
+                    <div class="prdctName">
+                        <h5>METAL ROLLS</h5>
+                        <a href="javascript::void()"><i class="fa-solid fa-arrow-right"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                <div class="product" data-aos="fade-up" data-aos-duration="1500">
+                    <img src="assets/img/engCastings.png" alt="worker icon">
+                    <div class="prdctName">
+                        <h5>Engineering Castings</h5>
+                        <a href="javascript::void()"><i class="fa-solid fa-arrow-right"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                <div class="product" data-aos="fade-left" data-aos-duration="1500">
+                    <img src="assets/img/steelRounds.png" alt="worker icon">
+                    <div class="prdctName">
+                        <h5>Alloy Steel Round & Flats</h5>
+                        <a href="javascript::void()"><i class="fa-solid fa-arrow-right"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- services-area-end -->
+
+    <!-- cta-area -->
+    <section class="cta__area fix">
+        <div class="cta__bg" data-background="assets/img/bg/cta_bg.jpg"></div>
+        <div class="container">
+            <div class="row align-items-end">
+                <div class="col-lg-8">
+                    <div class="cta__content">
+                        <h2 class="title">Ready to work with <br> our team?</h2>
+                        <div class="cta__btn">
+                            <a href="contact.php" class="btn transparent-btn">Contact With Us <img
+                                    src="assets/img/icons/right_arrow.svg" alt="" class="injectable"></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="cta__content-right" data-aos="fade-up" data-aos-delay="600">
+                        <h4 class="title">Leading Steel Manufacturing Company of INDIA</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="cta__shape">
+            <img src="assets/img/images/cta_shape.png" alt="shape" data-aos="fade-down-left" data-aos-delay="400">
+        </div>
+    </section>
+    <!-- cta-area-end -->
+    <div class="container footer__features">
+        <a href="javascript::void()">Metal Rolls</a>
+        <a href="javascript::void()">Engineering Castings</a>
+        <a href="javascript::void()">Alloy Steel Round & Flats</a>
+    </div>
+
+</main>
+<!-- main-area-end -->
+
+<?php include 'include/scripts.php'; ?>
+<?php include 'include/footer.php'; ?>
